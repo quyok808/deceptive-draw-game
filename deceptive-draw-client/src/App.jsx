3b2924f3c8fac5715bbe460b4d912da5e851c8af
CongQuy252 <quyok25203@gmail.com>
2025-07-13 00:01:12 +0700

    up

--- deceptive-draw-client/src/App.jsx
+++ deceptive-draw-client/src/App.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { ToastContainer, toast } from "react-toastify";
 
-const socket = io("http://localhost:3001");
+const socket = io(import.meta.env.VITE_SOCKET_URL);
 
 function App() {
   const [name, setName] = useState("");
